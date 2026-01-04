2bb2ca7d3b1e176e3b5db06b747139bce5533dbe
siam-khan-alt <nssiam99@gmail.com>
2026-01-04 18:15:35 +0600

    add user information update , realted vehicles for details page and user stats funtionality

--- index.js
+++ index.js
@@ -1,158 +1,228 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
-const app = express()
-const port = process.env.PORT || 3000
-app.use(cors())
-app.use(express.json())
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
 
-
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0sp.xsshgji.mongodb.net/?appName=Cluster0SP`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0sp.xsshgji.mongodb.net/?appName=Cluster0SP`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-
-app.get('/', (req, res)=>{
-    res.send('travel each')
-})
-
+app.get("/", (req, res) => {
+  res.send("travel each");
+});
 
 async function run() {
   try {
+    //  await client.connect();
 
-  //  await client.connect();
-
-    const db = client.db('travel_db')
+    const db = client.db("travel_db");
     // await client.db("admin").command({ ping: 1 });
-    
 
-       const userscollection = db.collection('users')
-       const vehiclescollection = db.collection('vehicles')
-       const bookingscollection = db.collection('bookings')
-    app.get('/users', async(req,res)=>{
-        const cursor = userscollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
-    })
-    app.post('/users', async(req, res)=>{
-        const newUser = req.body
-        const email = req.body.email
-        const query = {email: email}
-        const userexisting= await userscollection.findOne(query)
-        if(userexisting){
-          return res.status(400).send({ message: 'User already exists. Do not insert again!' });
+    const userscollection = db.collection("users");
+    const vehiclescollection = db.collection("vehicles");
+    const bookingscollection = db.collection("bookings");
+    app.get("/users", async (req, res) => {
+      const cursor = userscollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const email = req.body.email;
+      const query = { email: email };
+      const userexisting = await userscollection.findOne(query);
+      if (userexisting) {
+        return res
+          .status(400)
+          .send({ message: "User already exists. Do not insert again!" });
+      } else {
+        const result = await userscollection.insertOne(newUser);
+        res.send(result);
       }
-        else{
-         const result = await userscollection.insertOne(newUser)
-        res.send(result)
-        }
-        })
-        app.get('/vehicles', async(req, res)=>{
-      
-      const cursor = vehiclescollection.find()
-      const result = await cursor.toArray()
-      res.send(result)
-    })
-    app.get('/vehicles/users', async(req, res)=>{
-       const email = req.query.email
-        const query={}
-        if(email){
-            query.userEmail=email
-        }
-      const cursor = vehiclescollection.find(query)
-      const result = await cursor.toArray()
-      res.send(result)
-    })
-    app.get('/vehicles/latest', async(req, res)=>{
-      const cursor = vehiclescollection.find().sort({createdAt: -1}).limit(4)
-      const result = await cursor.toArray()
-      res.send(result)
-    })
-     app.get('/vehicles/top', async(req, res)=>{
-      const cursor = vehiclescollection.find().sort({bookingCount: -1}).limit(4)
-      const result = await cursor.toArray()
-      res.send(result)
-    })
-    app.get('/vehicles/:id', async(req, res)=>{
-     const id = req.params.id
-     const query = {_id: new ObjectId(id)}
-     const result = await vehiclescollection.findOne(query)
-     res.send(result)
-    })
-    app.post('/vehicles', async(req, res)=>{
-      const newVehicle= req.body
-      const result = await vehiclescollection.insertOne(newVehicle)
-      res.send(result)
-    })
-    app.delete('/vehicles/:id', async(req, res)=>{
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const result = await vehiclescollection.deleteOne(query)
-      res.send(result)
-    })
-    app.patch('/vehicles/:id', async(req, res)=>{
-      const updateVehicles = req.body
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const updatedata={
-        $set: updateVehicles
+    });
+    app.patch("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const { name, photo } = req.body;
+      const filter = { email: email };
+      const updatedDoc = {
+        $set: {
+          name: name,
+          photo: photo,
+        },
+      };
+      const result = await userscollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+    app.get("/vehicles", async (req, res) => {
+      const { search, category, sortBy } = req.query;
+      let query = {};
+      if (search) {
+        query = {
+          $or: [
+            { vehicleName: { $regex: search, $options: "i" } },
+            {
+              category: { $regex: search, $options: "i" },
+            },
+            { location: { $regex: search, $options: "i" } },
+          ],
+        };
       }
-      const result = await vehiclescollection.updateOne(query, updatedata)
-      res.send(result)
-    })
-     app.get('/bookings', async(req, res)=>{
-      console.log(req.query);
-      
-       const email = req.query.email
-        const query={}
-        if(email){
-            query.userEmail=email
-        }
-      const cursor = bookingscollection.find(query)
-      const result = await cursor.toArray()
-      res.send(result)
-    })
-    app.post('/bookings',async(req, res)=>{
-      const newBooking  = req.body
+      if (category) {
+        query.categories = category;
+      }
+
+      let sortQuery = {};
+      if (sortBy === "price-asc") sortQuery = { pricePerDay: 1 };
+      else if (sortBy === "price-desc") sortQuery = { pricePerDay: -1 };
+
+      const cursor = vehiclescollection.find(query).sort(sortQuery);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/vehicles/users", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.userEmail = email;
+      }
+      const cursor = vehiclescollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/vehicles/latest", async (req, res) => {
+      const cursor = vehiclescollection.find().sort({ createdAt: -1 }).limit(4);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/vehicles/top", async (req, res) => {
+      const cursor = vehiclescollection
+        .find()
+        .sort({ bookingCount: -1 })
+        .limit(4);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/vehicles/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await vehiclescollection.findOne(query);
+      res.send(result);
+    });
+    app.post("/vehicles", async (req, res) => {
+      const newVehicle = req.body;
+      const result = await vehiclescollection.insertOne(newVehicle);
+      res.send(result);
+    });
+    app.delete("/vehicles/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await vehiclescollection.deleteOne(query);
+      res.send(result);
+    });
+    app.patch("/vehicles/:id", async (req, res) => {
+      const updateVehicles = req.body;
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updatedata = {
+        $set: updateVehicles,
+      };
+      const result = await vehiclescollection.updateOne(query, updatedata);
+      res.send(result);
+    });
+    app.get("/related-vehicles", async (req, res) => {
+      const { category, currentId } = req.query;
+
       const query = {
-    userEmail: newBooking.userEmail,
-    vehicleId: newBooking.vehicleId
-  };
-  const filter = {_id: new ObjectId(newBooking.vehicleId)}
-  const updateCount ={
-    $inc:{
-      bookingCount:1
-    }
-  }
-      const vehcleexisting= await bookingscollection.findOne(query )
-      if(vehcleexisting){
-        return res.status(400).send({ message: 'You have already booked this vehicle!' })
+        categories: category,
+        _id: { $ne: new ObjectId(currentId) },
+      };
+
+      const result = await vehiclescollection.find(query).limit(3).toArray();
+
+      res.send(result);
+    });
+    app.get("/bookings", async (req, res) => {
+      console.log(req.query);
+
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.userEmail = email;
       }
-      else{
-      
-      const result = await bookingscollection.insertOne(newBooking);
-       await vehiclescollection.updateOne(filter, updateCount)
+      const cursor = bookingscollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/user-stats/:email", async (req, res) => {
+      const email = req.params.email;
+
+      const totalVehicles = await vehiclescollection.countDocuments({
+        userEmail: email,
+      });
+
+      const totalBookings = await bookingscollection.countDocuments({
+        userEmail: email,
+      });
+
+      const userVehicles = await vehiclescollection
+        .find({ userEmail: email })
+        .project({ vehicleName: 1, bookingCount: 1, pricePerDay: 1 })
+        .limit(5)
+        .toArray();
+
+      const chartData = userVehicles.map((v) => ({
+        name: v.vehicleName,
+        bookings: v.bookingCount || 0,
+        price: v.pricePerDay || 0,
+      }));
+
       res.send({
-      success: true,
-      insertedId: result.insertedId,
-    })
-  }})
-  
-  
+        totalVehicles,
+        totalBookings,
+        chartData,
+      });
+    });
+    app.post("/bookings", async (req, res) => {
+      const newBooking = req.body;
+      const query = {
+        userEmail: newBooking.userEmail,
+        vehicleId: newBooking.vehicleId,
+      };
+
+      const vehcleexisting = await bookingscollection.findOne(query);
+      if (vehcleexisting) {
+        return res
+          .status(400)
+          .send({ message: "You have already booked this vehicle!" });
+      } else {
+        const result = await bookingscollection.insertOne(newBooking);
+        const filter = { _id: new ObjectId(newBooking.vehicleId) };
+        const updateDoc = {
+          $inc: { bookingCount: 1 },
+        };
+
+        await vehiclescollection.updateOne(filter, updateDoc);
+        res.send({
+          success: true,
+          insertedId: result.insertedId,
+        });
+      }
+    });
   } finally {
-   
   }
 }
 run().catch(console.dir);
 
-app.listen(port, ()=>{
-    console.log('server connected', port);
-    
-})
+app.listen(port, () => {
+  console.log("server connected", port);
+});
